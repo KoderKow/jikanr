1ff6a8f5cb6878fbf9449641d07196820a10fc21
Kyle Harris <Kyle.Harris@claconnect.com>
2023-02-22 22:48:28 +0000

    updates

--- R/json_clean.R
+++ R/json_clean.R
@@ -1,27 +1,71 @@
 resp_json_clean <- function(resp) {
-  d <- tryCatch(
-    expr = {
-      resp %>%
-        httr2::resp_body_string() %>%
-        tidyjson::enter_object(data) %>%
-        tidyjson::gather_array() %>%
-        tidyjson::spread_all()
-    },
-    error = function(e) {
-      resp %>%
-        httr2::resp_body_string() %>%
-        tidyjson::enter_object(data) %>%
-        tidyjson::spread_all()
-    }
-  ) %>%
-    # Remove ..JSON column
-    dplyr::as_tibble() %>%
-    ## Remove other columns created by tidyjson
-    dplyr::select(
-      -dplyr::contains("document.id"),
-      -dplyr::contains("array.index")
-    ) %>%
-    janitor::clean_names()
+  l_init <-
+    resp %>%
+    httr2::resp_body_string() %>%
+    jsonlite::fromJSON(flatten = TRUE)
 
-  return(d)
+  d_init <-
+    l_init %>%
+    purrr::pluck("data") %>%
+    dplyr::as_tibble() %>%
+    dplyr::mutate(mal_id = mal_id) %>%
+    dplyr::relocate(mal_id)
+
+  d_dims_init <-
+    d_init %>%
+    dplyr::select(mal_id, where(is.list))
+
+  v_original_col_names <-
+    d_dims_init %>%
+    colnames() %>%
+    stringr::str_subset(
+      pattern = "^mal_id$",
+      negate = TRUE
+    )
+
+  d_dims_init <-
+    d_dims_init %>%
+    dplyr::rename_with(~ stringr::str_remove(.x, "s$"))
+
+  if (nrow(d_dims_init) > 1) {
+    max_col <- ncol(d_dims_init) - 1
+    i <- 1
+
+    l_dim_tables <- list()
+
+    while (i <= max_col) {
+      object <- names(d_dims_init)[i + 1]
+
+      d_step_1 <-
+        d_dims_init %>%
+        dplyr::select(1, i + 1) %>%
+        tidyr::unnest(
+          2,
+          # keep_empty = TRUE,
+          names_sep = "_"
+        )
+
+      l_dim_tables[[i]] <- d_step_1
+
+      i <- i + 1
+    }
+
+    l_dim_tables <- purrr::set_names(l_dim_tables, names(d_dims_init)[-1])
+
+    d_final <-
+      d_init %>%
+      dplyr::select(-dplyr::all_of(v_original_col_names)) %>%
+      janitor::clean_names()
+
+  } else {
+    d_final <- d_init
+    dim_tables <- NULL
+  }
+
+  l_final <- list(
+    d = d_final,
+    dim_tables = l_dim_tables
+  )
+
+  return(l_final)
 }
